2a7498c16b077839c722593034ee11d8de732eb7
Júnior  Nunes <denisonprobable13@gmail.com>
2023-09-17 15:27:13 -0300

    feat: add create transcription route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { fastify } from "fastify";
 import { prisma } from "./lib/prisma";
 import { getAllPromptsRoute } from "./routes/get-all-prompts.routes";
 import { uploadVideoRoute } from "./routes/upload-video.routes";
+import { createTranscriptionRoute } from "./routes/create-transcription.routes";
 
 const PORT = 3333;
 
@@ -9,6 +10,7 @@ const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
 
 app
   .listen({
