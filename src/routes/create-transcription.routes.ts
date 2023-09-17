3228ea7f311a279084f29e83912c635102cfd5de
Júnior  Nunes <denisonprobable13@gmail.com>
2023-09-17 15:46:43 -0300

    feat: connect with openai

--- src/routes/create-transcription.routes.ts
+++ src/routes/create-transcription.routes.ts
@@ -1,6 +1,8 @@
+import { createReadStream } from "node:fs";
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod";
+import { openAi } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post("/videos/:videoId/transcription", async (req) => {
@@ -16,9 +18,25 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 
     const { prompt } = bodySchema.parse(req.body);
 
-    return {
-      videoId,
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    const videoPath = video.path;
+
+    const audioReadStream = createReadStream(videoPath);
+
+    const response = await openAi.audio.transcriptions.create({
+      file: audioReadStream,
+      model: "whisper-1",
+      language: "en",
+      response_format: "json",
+      temperature: 0,
       prompt,
-    };
+    });
+
+    return response.text;
   });
 }
