7d13076ae135439dc258649e4fbdc9265edac6bd
Júnior  Nunes <denisonprobable13@gmail.com>
2023-09-17 15:08:44 -0300

    feat: making audio upload to a temp path

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { fastify } from "fastify";
 import { prisma } from "./lib/prisma";
 import { getAllPromptsRoute } from "./routes/get-all-prompts.routes";
+import { uploadVideoRoute } from "./routes/upload-video.routes";
 
 const PORT = 3333;
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
 app
   .listen({
