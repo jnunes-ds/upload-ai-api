9b614e9c1de34f6d223bea4b3a4541ec78f443dc
Júnior  Nunes <denisonprobable13@gmail.com>
2023-09-17 13:48:13 -0300

    feat: using prisma to build our db and make our first get request

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import { fastify } from "fastify";
+import { prisma } from "./lib/prisma";
+import { getAllPromptsRoute } from "./routes/get-all-prompts.routes";
 
 const PORT = 3333;
 
 const app = fastify();
 
-app.get('/', () =>  'Hello World');
+app.register(getAllPromptsRoute);
 
-app.listen( {
-    port: PORT
-}).then(() =>  {
-    console.log('Http server running on port ', PORT);
-})
+app
+  .listen({
+    port: PORT,
+  })
+  .then(() => {
+    console.log("Http server running on port ", PORT);
+  });
